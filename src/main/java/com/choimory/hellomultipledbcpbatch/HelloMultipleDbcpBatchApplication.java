9db8d63fcde1f183756b3ec8351f45e623cc58ca
YunjoongChoi <83948750+YunjoongChoi@users.noreply.github.com>
2021-06-21 14:55:55 +0900

    Initial commit

---
+++ src/main/java/com/choimory/hellomultipledbcpbatch/HelloMultipleDbcpBatchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.hellomultipledbcpbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMultipleDbcpBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMultipleDbcpBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/hellomultipledbcpbatch/HelloMultipleDbcpBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.hellomultipledbcpbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMultipleDbcpBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
