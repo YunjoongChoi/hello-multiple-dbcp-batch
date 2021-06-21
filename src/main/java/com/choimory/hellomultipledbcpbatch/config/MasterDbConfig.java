ea6b47c595155b6e809d2b6d8ee68d4b8e001a69
YunjoongChoi <83948750+YunjoongChoi@users.noreply.github.com>
2021-06-21 19:17:42 +0900

    spring.jpa.hibernate, spring.jpa.properties 적용 외 완성

--- src/main/java/com/choimory/hellomultipledbcpbatch/config/MasterDbConfig.java
+++ src/main/java/com/choimory/hellomultipledbcpbatch/config/MasterDbConfig.java
@@ -1,67 +1,81 @@
 package com.choimory.hellomultipledbcpbatch.config;
 
-import lombok.RequiredArgsConstructor;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Configuration
-@EnableJpaRepositories(entityManagerFactoryRef = "masterEntityManager", transactionManagerRef = "masterTransactionManager", basePackages = {"com.choimory.hellomultipledbcpbatch.master.repository"})
-@EnableTransactionManagement
-@RequiredArgsConstructor
+@EnableConfigurationProperties(JpaProperties.class)
+@EnableJpaRepositories(basePackages = MasterDbConfig.PACKAGE, entityManagerFactoryRef = MasterDbConfig.ENTITY_MANAGER, transactionManagerRef = MasterDbConfig.TRANSACTION_MANAGER)
 public class MasterDbConfig {
-    private final Environment environment;
+    public static final String PACKAGE = "com.choimory.hellomultipledbcpbatch.master";
+    public static final String ENTITY_MANAGER = "masterEntityManager";
+    public static final String TRANSACTION_MANAGER = "masterTransactionManager";
+    public static final String DATA_SOURCE = "masterDataSource";
+    public static final String VENDOR_ADAPTER = "masterJpaVendorAdapter";
+    public static final String DATA_SOURCE_PROPERTIES = "masterDataSourceProperties";
 
     @Primary
-    @Bean
+    @Bean(DATA_SOURCE_PROPERTIES)
     @ConfigurationProperties(prefix = "spring.datasource.master")
-    public DataSource masterDataSource(){
-        return DataSourceBuilder.create().build();
+    public DataSourceProperties dataSourceProperties(){
+        return new DataSourceProperties();
     }
 
     @Primary
-    @Bean
-    public LocalContainerEntityManagerFactoryBean masterEntityManager(){
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-
-        //data source
-        entityManager.setDataSource(masterDataSource());
-
-        //entity
-        entityManager.setPackagesToScan(new String[]{"com.choimory.hellomultipledbcpbatch.master.entity"});
-
-        //hibernate
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        entityManager.setJpaVendorAdapter(vendorAdapter);
-
-        //hibernate config
-        HashMap<String, Object> props = new HashMap<>();
-        props.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
-        props.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
-        entityManager.setJpaPropertyMap(props);
-
-        return entityManager;
+    @Bean(DATA_SOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    public DataSource dataSource(){
+        return dataSourceProperties()
+                .initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
     }
 
     @Primary
-    @Bean
-    public PlatformTransactionManager masterTransactionManager(){
+    @Bean(VENDOR_ADAPTER)
+    public JpaVendorAdapter jpaVendorAdapter(@Qualifier(DATA_SOURCE) DataSource dataSource, JpaProperties jpaProperties){
+        AbstractJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(jpaProperties.isShowSql());
+        adapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+        return adapter;
+    }
+
+    @Primary
+    @Bean(ENTITY_MANAGER)
+    public LocalContainerEntityManagerFactoryBean entityManageFactory(@Qualifier(VENDOR_ADAPTER) JpaVendorAdapter jpaVendorAdapter
+                                                                        , ObjectProvider<PersistenceUnitManager> persistenceUnitManager
+                                                                        , JpaProperties jpaProperties
+                                                                        , @Qualifier(DATA_SOURCE) DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean obj = new EntityManagerFactoryBuilder(jpaVendorAdapter, jpaProperties.getProperties(), persistenceUnitManager.getIfAvailable()).dataSource(dataSource)
+                                                                                                                                        .packages(PACKAGE)
+                                                                                                                                        .build();
+        return obj;
+    }
+
+    @Primary
+    @Bean(TRANSACTION_MANAGER)
+    public PlatformTransactionManager transactionManager(@Qualifier(ENTITY_MANAGER) LocalContainerEntityManagerFactoryBean entityManager){
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-
-        transactionManager.setEntityManagerFactory(masterEntityManager().getObject());
-
+        transactionManager.setEntityManagerFactory(entityManager.getObject());
         return transactionManager;
     }
 }
--- src/main/java/com/choimory/hellomultipledbcpbatch/config/SlaveDbConfig.java
+++ src/main/java/com/choimory/hellomultipledbcpbatch/config/SlaveDbConfig.java
@@ -1,63 +1,74 @@
 package com.choimory.hellomultipledbcpbatch.config;
 
-import lombok.RequiredArgsConstructor;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Configuration
-@EnableJpaRepositories(entityManagerFactoryRef = "slaveEntityManager", transactionManagerRef = "slaveTransactionManager", basePackages = {"com.choimory.hellomultipledbcpbatch.slave.repository"})
-@EnableTransactionManagement
-@RequiredArgsConstructor
+@EnableConfigurationProperties(JpaProperties.class)
+@EnableJpaRepositories(basePackages = SlaveDbConfig.PACKAGE, entityManagerFactoryRef = SlaveDbConfig.ENTITY_MANAGER, transactionManagerRef = SlaveDbConfig.TRANSACTION_MANAGER)
 public class SlaveDbConfig {
-    private final Environment environment;
+    public static final String PACKAGE = "com.choimory.hellomultipledbcpbatch.slave";
+    public static final String ENTITY_MANAGER = "slaveEntityManager";
+    public static final String TRANSACTION_MANAGER = "slaveTransactionManager";
+    public static final String DATA_SOURCE = "slaveDataSource";
+    public static final String VENDOR_ADAPTER = "slaveJpaVendorAdapter";
+    public static final String DATA_SOURCE_PROPERTIES = "slaveDataSourceProperties";
 
-    @Bean
+    @Bean(DATA_SOURCE_PROPERTIES)
     @ConfigurationProperties(prefix = "spring.datasource.slave")
-    public DataSource slaveDataSource(){
-        return DataSourceBuilder.create().build();
+    public DataSourceProperties dataSourceProperties(){
+        return new DataSourceProperties();
     }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean slaveEntityManager(){
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-
-        //data source
-        entityManager.setDataSource(slaveDataSource());
-
-        //entity
-        entityManager.setPackagesToScan(new String[]{"com.choimory.hellomultipledbcpbatch.slave.entity"});
-
-        //hibernate
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        entityManager.setJpaVendorAdapter(vendorAdapter);
-
-        //hibernate config
-        HashMap<String, Object> props = new HashMap<>();
-        props.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
-        props.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
-        entityManager.setJpaPropertyMap(props);
-
-        return entityManager;
+    @Bean(DATA_SOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+    public DataSource dataSource(){
+        return dataSourceProperties()
+                .initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
     }
 
-    @Bean
-    public PlatformTransactionManager slaveTransactionManager(){
+    @Bean(VENDOR_ADAPTER)
+    public JpaVendorAdapter jpaVendorAdapter(@Qualifier(DATA_SOURCE) DataSource dataSource, JpaProperties jpaProperties){
+        AbstractJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(jpaProperties.isShowSql());
+        adapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+        return adapter;
+    }
+
+    @Bean(ENTITY_MANAGER)
+    public LocalContainerEntityManagerFactoryBean entityManageFactory(@Qualifier(VENDOR_ADAPTER) JpaVendorAdapter jpaVendorAdapter
+            , ObjectProvider<PersistenceUnitManager> persistenceUnitManager
+            , JpaProperties jpaProperties
+            , @Qualifier(DATA_SOURCE) DataSource dataSource){
+        return new EntityManagerFactoryBuilder(jpaVendorAdapter, jpaProperties.getProperties(), persistenceUnitManager.getIfAvailable()).dataSource(dataSource)
+                .packages(PACKAGE)
+                .build();
+    }
+
+    @Bean(TRANSACTION_MANAGER)
+    public PlatformTransactionManager transactionManager(@Qualifier(ENTITY_MANAGER) LocalContainerEntityManagerFactoryBean entityManager){
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-
-        transactionManager.setEntityManagerFactory(slaveEntityManager().getObject());
-
+        transactionManager.setEntityManagerFactory(entityManager.getObject());
         return transactionManager;
     }
 }
